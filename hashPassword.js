ba7c08d1d4b207fb3fa4158da7786282f927d678
elcioix <101727887+elcioix@users.noreply.github.com>
2024-11-13 20:54:10 -0300

    Delete hashPassword.js

--- hashPassword.js
+++
@@ -1,9 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-async function generatePasswordHash() {
-  const password = "aSenha"; //  (exTensaoFernanda$)
-  const hashedPassword = await bcrypt.hash(password, 10);
-  console.log("Senha criptografada:", hashedPassword);
-}
-
-generatePasswordHash();
